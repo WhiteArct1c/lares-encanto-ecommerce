51797ae106e18933a7a2563cc5cd74e28a298299
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2024-03-11 22:16:24 -0300

    refactor - creating a component to sidenav on user or admin profile

---
+++ src/shared/MyProfileSidenavComponent/index.tsx
@@ -0,0 +1,50 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import {Button, Divider} from "@mui/material";
+import React from "react";
+import {Link} from "react-router-dom";
+
+interface MyProfileSidenavComponentProps{
+}
+const MyProfileSidenavComponent: React.FC<MyProfileSidenavComponentProps> = () => {
+    return(
+        <Grid2
+            container
+            xs={2}
+            spacing={2}
+        >
+            <Grid2 sx={{ pl: 5 }}>
+                <Divider />
+                <Button
+                    variant='text'
+                    sx={{ color: 'black', m: 1 }}
+                    component={Link}
+                    to={'/my-profile'}
+                >
+                    Minhas Informações
+                </Button>
+                <Button
+                    variant='text'
+                    sx={{ color: 'black', m: 1 }}
+                    component={Link}
+                    to={'/my-orders'}
+                >
+                    Meus Pedidos
+                </Button>
+                <Button
+                    variant='text'
+                    sx={{ color: 'black', m: 1 }}
+                >
+                    Meus cupons
+                </Button>
+                <Button
+                    variant='text'
+                    sx={{ color: 'black', m: 1 }}
+                >
+                    Meus cartões
+                </Button>
+            </Grid2>
+        </Grid2>
+    );
+};
+
+export default MyProfileSidenavComponent;
