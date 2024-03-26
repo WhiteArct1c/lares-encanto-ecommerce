92d22236d4af7cdd354f14406b2c3e78f7262ffc
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2024-03-26 02:32:48 -0300

    refactor: adjusting scrollbar style

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -3,30 +3,29 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
-   *::-webkit-scrollbar{
-      width: 15px;
-      background: #000;
-      border-radius: 5px;
-   }
+    *::-webkit-scrollbar {
+        width: 7px;
+        background: #fff;
+        border-radius: 2px;
+    }
 
-   *::-webkit-scrollbar-thumb {
-      background: #fff; 
-      border-radius: 5px;
-      border: 3px solid black
-   }
+    *::-webkit-scrollbar-thumb {
+        background: rgba(44, 44, 44, 0.99);
+        border-radius: 10px;
+    }
 
-   *::-webkit-scrollbar-button {
-      display: none; 
-   }
+    *::-webkit-scrollbar-button {
+        display: none;
+    }
 
-   body{
-      padding: 0;
-      margin: 0;
-      box-sizing: border-box;
+    body {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
 
-      background-color: #EFF2F6;
+        background-color: #EFF2F6;
 
-      font-family: 'Public Sans', sans-serif;
-      font-weight: 400;
-   }
+        font-family: 'Public Sans', sans-serif;
+        font-weight: 400;
+    }
 `
