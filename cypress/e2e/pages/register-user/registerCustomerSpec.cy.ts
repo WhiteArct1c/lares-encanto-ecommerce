9a5437206c7d5d04edb3158ab8cd0cb964964981
WhiteArct1c <matrodrigues1576@gmail.com>
2024-03-26 16:11:30 -0300

    feature - add new testes for the register customer page

--- cypress/e2e/pages/register-user/registerCustomerSpec.cy.ts
+++ cypress/e2e/pages/register-user/registerCustomerSpec.cy.ts
@@ -21,7 +21,7 @@ describe('Register Customer', () => {
     expect(cy.contains('p', 'O Estado é obrigatório'));
   });
 
-  it('Submit the register form with a invalid password', () => {
+  it('Submit the register form with a non-equal password', () => {
 
     cy.get('input[name="password"]').type('Mat15766@');
     cy.get('input[name="confirmedPassword"]').type('Mat15766');
@@ -30,6 +30,56 @@ describe('Register Customer', () => {
     expect(cy.contains('p', 'As senhas devem ser iguais!'));
 
   });
+
+  it('Submit the register form with a password without uppercase, special character and numbers', () => {
+    cy.get('input[name="fullName"]').type('Jaime Almeida');
+    cy.get('input[name="cpf"]').type('47191502843');
+    cy.get('input[name="birthDate"]').click();
+    cy.get('input[name="birthDate"]').type('1999-06-25');
+    cy.get('input[name="phone"]').type('11950691542');
+    cy.get('input[name="gender"][value="MASCULINO"]').click();
+    cy.get('input[name="email"]').type(`matheusbispo@gmail.com`);
+    cy.get('textarea[name="addressTitle"]').type('Casa Teste');
+    cy.get('input[name="cep"]').type('08552330').blur();
+    cy.wait(1500);
+    cy.get('input[name="addressNumber"]').type('635');
+    cy.get('input[name="password"]').type('matmatmat');
+    cy.get('input[name="confirmedPassword"]').type('matmatmat');
+    cy.wait(1500);
+    cy.contains('button', 'Cadastrar').click();
+
+    expect(
+        cy.get('div[role="alert"]')
+            .should('be.visible')
+            .should('have.text', 'A senha deve conter pelo menos uma letra maiúscula. - A senha deve conter pelo menos um caractere especial. - ')
+    )
+  })
+
+  it('Submit the register form with a password without 8 characters at least', () => {
+    cy.get('input[name="fullName"]').type('Jaime Almeida');
+    cy.get('input[name="cpf"]').type('47191502843');
+    cy.get('input[name="birthDate"]').click();
+    cy.get('input[name="birthDate"]').type('1999-06-25');
+    cy.get('input[name="phone"]').type('11950691542');
+    cy.get('input[name="gender"][value="MASCULINO"]').click();
+    cy.get('input[name="email"]').type(`matheusbispo@gmail.com`);
+    cy.get('textarea[name="addressTitle"]').type('Casa Teste');
+    cy.get('input[name="cep"]').type('08552330').blur();
+    cy.wait(1500);
+    cy.get('input[name="addressNumber"]').type('635');
+    cy.get('input[name="password"]').type('Mat157@');
+    cy.get('input[name="confirmedPassword"]').type('Mat157@');
+    cy.wait(1500);
+    cy.contains('button', 'Cadastrar').click();
+
+    expect(
+        expect(
+            cy.contains('p', 'A senha deve ter no mínimo 8 dígitos.')
+                .should('be.visible')
+                .should('have.text', 'A senha deve ter no mínimo 8 dígitos.')
+        )
+    )
+  })
 
   it('Register a customer with no errors and validate if was really created', () => {
     cy.get('input[name="fullName"]').type('Matheus Rodrigues Bispo');
@@ -48,13 +98,19 @@ describe('Register Customer', () => {
     cy.wait(1500);
     cy.contains('button', 'Cadastrar').click();
 
-    expect(cy.get('div[role="alert"]').contains('Cadastro concluído com sucesso'));
+    expect(
+        cy.get('div[role="alert"]').contains('Cadastro concluído com sucesso')
+            .should('be.visible')
+    );
 
     //validate if the new user was created
     cy.get('input[name="email"]').type('matheusbispo@gmail.com');
     cy.get('input[name="password"]').type('Mat15766@');
     cy.contains('button', 'Entrar').click();
 
-    expect(cy.get('div[role="alert"]').contains('Login efetuado com sucesso!'));
+    expect(
+        cy.get('div[role="alert"]').contains('Login efetuado com sucesso!')
+            .should('be.visible')
+    );
   });
 })
