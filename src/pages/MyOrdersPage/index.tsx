47c552e055e42dbe9c9ecc2d218f5747cf418d7c
WhiteArct1c <matrodrigues1576@gmail.com>
2024-03-30 18:07:16 -0300

    feat: create the new DataGrid which contains the customer order items

--- src/pages/MyOrdersPage/index.tsx
+++ src/pages/MyOrdersPage/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import {Accordion, AccordionActions, AccordionDetails, AccordionSummary, Box, Button, Typography} from "@mui/material";
+import React, {useState} from "react";
+import {
+    Accordion,
+    AccordionActions,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Button,
+    Dialog, DialogActions, DialogContent, DialogTitle,
+    Typography
+} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import MyProfileSidenavComponent from "../../shared/MyProfileSidenavComponent";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import {DataGrid, GridColDef} from '@mui/x-data-grid';
 import {ExpandMore} from "@mui/icons-material";
+import ChangeItemsFormComponent from "./components/change-items-form-component.tsx";
+import DevolutionFormComponent from "./components/devolution-form-component.tsx";
 
 interface MyOrdersPageProps{
 
@@ -27,13 +38,37 @@ const columns: GridColDef[] = [
     }
 ];
 
-const rows = [
+export interface RowData{
+    id: number;
+    productName: string;
+    productQtd: number;
+}
+
+const rows: RowData[] = [
     {id: 1, productName: 'Cadeira Flex', productQtd: 1},
     {id: 2, productName: 'Sofá Urban Black', productQtd: 1},
-    {id: 3, productName: 'Mesa Ego White', productQtd: 1}
+    {id: 3, productName: 'Sofá Urban Black', productQtd: 1},
+    {id: 4, productName: 'Sofá Urban Black', productQtd: 1},
+    {id: 5, productName: 'Sofá Urban Black', productQtd: 1},
+    {id: 6, productName: 'Sofá Urban Black', productQtd: 1},
+    {id: 7, productName: 'Mesa Ego White', productQtd: 1}
 ];
 
 const MyOrdersPage: React.FC<MyOrdersPageProps> = () => {
+
+    const [open, setOpen] = useState(false);
+    const [titleDialog, setTitleDialog] = useState('');
+    const [selectedRows, setSelectedRows] = useState<RowData[]>([]);
+
+    const handleClickOpenDialog = (value: string) => {
+        setTitleDialog(value)
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return(
         <Grid2 container xs={12} sx={{mb: 10}}>
             <Grid2 xs={12} sx={{ pl: 2,  mt: 15}}>
@@ -65,18 +100,34 @@ const MyOrdersPage: React.FC<MyOrdersPageProps> = () => {
                                         },
                                     },
                                 }}
-                                pageSizeOptions={[5]}
                                 checkboxSelection
-                                disableRowSelectionOnClick
+                                onRowSelectionModelChange={(ids) => {
+                                    const selectedRowData = rows.filter(row => ids.includes(row.id))
+                                    setSelectedRows(selectedRowData);
+                                }}
                             />
                         </Box>
                     </AccordionDetails>
                     <AccordionActions>
-                        <Button sx={{ color: 'black', m: 1 }}>Trocar</Button>
-                        <Button sx={{ color: 'black', m: 1 }}>Devolver itens</Button>
+                        <Button sx={{ color: 'black', m: 1 }} onClick={() => handleClickOpenDialog('Troca de produtos')}>Trocar items</Button>
+                        <Button sx={{ color: 'black', m: 1 }} onClick={() => handleClickOpenDialog('Devolução de produtos')}>Devolver items</Button>
                     </AccordionActions>
                 </Accordion>
             </Grid2>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle sx={{display: 'flex', justifyContent:'center', alignItems:'center'}}>{titleDialog}</DialogTitle>
+                <DialogContent>
+                    {
+                        titleDialog === 'Troca de produtos' ?
+                            <ChangeItemsFormComponent/>
+                            :
+                            <DevolutionFormComponent items={selectedRows}/>
+                    }
+                    <DialogActions>
+                    </DialogActions>
+                </DialogContent>
+            </Dialog>
         </Grid2>
     );
 }
