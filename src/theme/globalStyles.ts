49d863028b7d59dd129fdbaa6edb3882cf7b6450
WhiteArct1c <matrodrigues1576@gmail.com>
2024-08-07 15:28:54 -0300

    style: adjusting global style about margin and padding

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -3,6 +3,12 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
     *::-webkit-scrollbar {
         width: 7px;
         background: #fff;
