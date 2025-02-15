95e2a0feac952e78728f4670d5b64e32e4622118
WhiteArct1c <matrodrigues1576@gmail.com>
2025-02-15 19:03:56 -0300

    feat: add a subtitle, AI mascot and presentation component for LaresAI

--- src/shared/AISuggestionsDialogComponent/PresentationStepComponent/index.tsx
+++ src/shared/AISuggestionsDialogComponent/PresentationStepComponent/index.tsx
@@ -1,13 +1,47 @@
 import React from "react";
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import 'animate.css';
+import {AutoAwesome} from "@mui/icons-material";
 
 interface PresentationStepComponentProps {}
 
 const PresentationStepComponent: React.FC<PresentationStepComponentProps> = () => {
     return (
-        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', padding: 5 }}>
-            <img src='../../../assets/laresAi/SVG/laresAI%20-%20normal.svg' alt='laresAi' style={{ width: '200px' }} />
+        <Box sx={{
+            width: '100%',
+            display: 'flex',
+            flexDirection:'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingRight: 7,
+            paddingLeft: 7,
+            gap: 2
+        }}>
+            <img
+                src='src/assets/laresAi/SVG/laresAI%20-%20normal.svg'
+                alt='laresAi'
+                style={{
+                    width: '200px',
+                    filter: 'drop-shadow(0 10px 5px rgba(0, 0, 0, 0.3))'
+                }}
+                className='animate__animated animate__fadeIn'
+            />
+            <Typography
+                sx={{
+                    width: '100%',
+                    textAlign: 'center',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 2,
+                    marginBottom: 2
+                }}
+                className='animate__animated animate__fadeIn animate__delay-1s'
+            >
+                powered by <AutoAwesome color='primary'/> Google Gemini.
+            </Typography>
         </Box>
+
     );
 }
 
